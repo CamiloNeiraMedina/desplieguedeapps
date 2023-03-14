e7beb46cc9bd12c35d45913eceac5826061479c7
CamiloNeiraMedina <86502849+CamiloNeiraMedina@users.noreply.github.com>
2023-03-13 19:48:24 -0600

    Add files via upload

---
+++ scripts/templates/db.cneiramedina.com.js
@@ -0,0 +1,10 @@
+$TTL 86400
+@       IN      SOA     www.cneiramedina.com. admin.cneiramedina.com. (
+                        1       ; serial
+                        604800  ; refresh
+                        86400   ; retry
+                        2419200 ; expire
+                        86400 ) ; minimum
+
+@       IN      NS      www.cneiramedina.com.
+www     IN      A       192.168.1.10
